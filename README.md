b46b0d546ea6130baa7bd300b63d3d8a55b2cd53
Nafrin Ilma Abdullah <nafrinilma@Nafrins-MacBook-Air.local>
2021-10-12 16:00:37 -0400

    added some content

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # express-portfolio-site
 COMP229– Web Application Development - Assignment 1
+
+
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
---
+++ public/javascripts/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { title: 'Home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', 
+  { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about-me', function(req, res, next) {
+  res.render('about', 
+  { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', 
+  { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', 
+  { title: 'Services' });
+});
+
+/* GET contact me page. */
+router.get('/contact-me', function(req, res, next) {
+  res.render('contact', 
+  { title: 'Contact' });
+});
+
+module.exports = router;
