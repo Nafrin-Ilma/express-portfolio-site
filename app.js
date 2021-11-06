9303976b37ca3a9082d111d562e3f66648071d3b
Nafrin Ilma Abdullah <nafrinilma@Nafrins-MacBook-Air.local>
2021-11-06 17:23:07 -0400

    new packages installed

--- app.js
+++ app.js
@@ -4,7 +4,12 @@
     Student ID: 301156573
     Date: 11/10/2021
 */
-const express = require("express");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
 const app = express();
 
 // get static files
@@ -12,14 +17,33 @@ app.use(express.static('public'));
 
 // set view engine
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 // get routes index page
 app.get('/', (req, res, next) => {
     res.render('index');
 });
 
+app.post('/login', function(req, res) {
+    res.render('login');
+  });
+
 const viewRoutes = require('./public/javascripts/index');
 
 app.use('/', viewRoutes);
+
+// catch 404
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error', { title: 'Error' });
+});
 
 app.listen(process.env.PORT || 4200);
